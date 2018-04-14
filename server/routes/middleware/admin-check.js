27001b7bd6d53a20cf4ed63b36ace2eae840dc71
dancinturtle <amy.giver@gmail.com>
2018-04-13 17:16:58 -0700

    fixed subission

--- server/routes/middleware/admin-check.js
+++ server/routes/middleware/admin-check.js
@@ -10,12 +10,12 @@
 // }),
 
 function adminCheck(request, response, next) {
-  if (req.session.admin) {
-    console.log('user ' + req.session.userId + ' is admin');
+  if (request.session.admin) {
+    console.log('user ' + request.session.userId + ' is admin');
     return next();
   }
-  console.log('user ' + req.session.userId + ' is not admin');
-  res.status(401).send('Not an admin');
+  console.log('user ' + request.session.userId + ' is not admin');
+  response.status(401).send('Not an admin');
 }
 
 module.exports = adminCheck;
--- server/routes/middleware/team-check.js
+++ server/routes/middleware/team-check.js
@@ -10,12 +10,12 @@
 // });
 
 function teamCheck(request, response, next) {
-  if (req.session.team) {
-    console.log('user ' + req.session.userId + ' is a team');
+  if (request.session.team) {
+    console.log('user ' + request.session.userId + ' is a team');
     return next();
   }
-  console.log('user ' + req.session.userId + ' is not a team');
-  res.status(401).send('Not a team');
+  console.log('user ' + request.session.userId + ' is not a team');
+  response.status(401).send('Not a team');
 }
 
 module.exports = teamCheck;
